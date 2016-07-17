b7ff2f3cc4696b29cd37e32fc8e6836037e33e72
Nathan McElwain <nathanmcelwain@ymail.com>
2016-07-16 17:28:24 -0700

    more completion

--- _build/js/main.js
+++ _build/js/main.js
@@ -12,7 +12,8 @@
         price: 10,
         description: 'This is a 10 sided gem object, and a secret piece of knowledge Lorem ipsum dolor sit amet',
         canPurchase: true,
-        soldOut: false
+        soldOut: false,
+        images: [{full: 'assets/img/dode.jpg'}]
      },
      {
        type: 'gem',
@@ -20,7 +21,8 @@
        price: 4,
        description: 'This is a 4 sided gem object, and a secret piece of knowledge Lorem ipsum dolor sit amet',
        canPurchase: false ,
-       soldOut: false
+       soldOut: false,
+       images: [{full: 'assets/img/tetra.jpg'}]
     },
     {
       type: 'gem',
@@ -28,7 +30,8 @@
       price: 8,
       description: 'This is a 8 sided gem object, and a secret piece of knowledge Lorem ipsum dolor sit amet',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [{full: 'assets/img/octo.jpg'}]
    },
    {
      type: 'gem',
@@ -36,7 +39,7 @@
      price: 6,
      description: 'This is a 6 sided gem object, and a secret piece of knowledge Lorem ipsum dolor sit amet',
      canPurchase: true,
-     soldOut: false
-  },
-    ];
+     soldOut: false,
+     images: [{full: 'assets/img/isos.jpg'}]
+  }];
 })();
