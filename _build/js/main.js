97aaa78a7f17003694ec01c1d468b63542ccafa0
Nathan McElwain <nathanmcelwain@ymail.com>
2016-07-18 10:50:38 -0700

    working on stuff

--- _build/js/main.js
+++ _build/js/main.js
@@ -4,6 +4,17 @@
   app.controller('StoreCtrl', function(){
     this.products = gems;
   });
+  app.controller('PanelCtrl', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
 
   var gems = [
     {
@@ -13,7 +24,12 @@
         description: 'This is a 10 sided gem object, and a secret piece of knowledge Lorem ipsum dolor sit amet',
         canPurchase: true,
         soldOut: false,
-        images: [{full: 'assets/img/dode.jpg'}]
+        images: [{full: 'assets/img/dode.jpg'}],
+        reviews:{
+            rating: 5,
+            text: "I love this gem, it's just wonderful",
+            user: "mary@lovesit.com"
+          }
      },
      {
        type: 'gem',
@@ -22,7 +38,12 @@
        description: 'This is a 4 sided gem object, and a secret piece of knowledge Lorem ipsum dolor sit amet',
        canPurchase: false ,
        soldOut: false,
-       images: [{full: 'assets/img/tetra.jpg'}]
+       images: [{full: 'assets/img/tetra.jpg'}],
+       reviews:{
+           rating: 5,
+           text: "I love this gem, it's just wonderful",
+           user: "mary@lovesit.com"
+         },
     },
     {
       type: 'gem',
@@ -31,7 +52,12 @@
       description: 'This is a 8 sided gem object, and a secret piece of knowledge Lorem ipsum dolor sit amet',
       canPurchase: true,
       soldOut: false,
-      images: [{full: 'assets/img/octo.jpg'}]
+      images: [{full: 'assets/img/octo.jpg'}],
+      reviews:{
+          rating: 5,
+          text: "I love this gem, it's just wonderful",
+          user: "mary@lovesit.com"
+        },
    },
    {
      type: 'gem',
@@ -40,6 +66,11 @@
      description: 'This is a 6 sided gem object, and a secret piece of knowledge Lorem ipsum dolor sit amet',
      canPurchase: true,
      soldOut: false,
-     images: [{full: 'assets/img/isos.jpg'}]
+     images: [{full: 'assets/img/isos.jpg'}],
+     reviews:{
+         rating: 5,
+         text: "I love this gem, it's just wonderful",
+         user: "mary@lovesit.com"
+       },
   }];
 })();
